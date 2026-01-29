577f7d97fd46a851e34ec97eeee4d7dee815532e
Pratham Joshi <prathamjoshi11@proton.me>
2026-01-30 00:35:36 +0530

    Modularised the board.py file. Need to continue working on it and fix a few issues especially in exception handling.

--- core/board.py
+++ core/board.py
@@ -1,40 +1,48 @@
 import chess
 
-is_fen = bool(input("Do you want to import a FEN?:"))
-if(is_fen):
-    fen=str(input("Enter fen: "))
-    Board1=chess.Board(fen)
-else:
-    Board1=chess.Board()
-print(Board1)
-while(1):
-    print(Board1)
-    try:
-        string=str(input("Enter your move: "))
+class Board:
+    def __init__(self, fen=None):
+        if(fen):
+            self.board=chess.Board(fen)
+        else:
+            self.board=chess.Board()
+
+    def print_board(self):
+        print(self.board)
+
+    def make_move(self,uci):
         try:
-            if (string=="undo"):
-                Board1.pop()
-                print("Successful Undo Operation Performed!")
-                continue
+            move=chess.Move.from_uci(uci)
+        except chess.InvalidMoveError:
+            print("Invalid! Please Enter correct UCI string.")
+        if(move in self.board.legal_moves):
+            self.board.push(move)
+            print("Legal Move")
+        else:
+            print("Illegal Move")
+        
+            
+    def undo_move(self):
+        try:
+            self.board.pop()
+            print("Successful Undo Operation Performed!")
         except IndexError:
             print("No chess history exists! Play a move first.")
-            continue
-        if(string=="get_fen"):
-            b1_fen=Board1.fen()
-            print("FEN aquired in b1_fen variable!")
-            continue
-        move=chess.Move.from_uci(string)
 
-    except chess.InvalidMoveError:
-        print("Invalid! Please Enter correct UCI string.")
-        continue
+    def get_fen(self):
+        return self.board.fen()
+    
+    def load_fen(self,fen):
+        self.board=chess.Board(fen)
+        
 
-    if(move in Board1.legal_moves):
-        Board1.push(move)
-        print("Legal Move")
+"""
 
-    else:
-        print("Illegal Move")
+
+    
+
+    
         print(Board1.move_stack)
 
 
+"""
