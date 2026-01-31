4e97159cda1f8499f6e8f8d25ae681bb8228a2cc
Pratham Joshi <prathamjoshi11@proton.me>
2026-01-31 15:26:57 +0530

    Intgrated stockfish and the board into main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from core.board import Board
+from engine.stockfish_engine import get_best_move
 
 choice=int(input("Enter 1 to start new game and 2 to load a FEN. "))
 if (choice==1):
@@ -12,24 +13,32 @@ else:
     print("Invalid input!")
 
 choice2=0
-while(choice2!=5):
-    choice2=int(input("1. Make a move\n2.Undo move\n3.Get FEN of the board\n4.Change state of board to new FEN\n5.Exit"))
+while(choice2!=6):
+    choice2=int(input("1. Make a move\n2. Let Stockfish make a move\n3. Undo move\n4. Get FEN of the board\n5. Change state of board to new FEN\n6. Exit"))
     if choice2==1:
         uci=str(input("Enter UCI string: "))
         resmkmv=b.make_move(uci)
         if(resmkmv==False):
             print("Invalid input!")
         b.print_board()
-    if choice2==2:
+    elif choice2==2:
+        move=get_best_move(b.get_fen())
+        success=b.make_move(move)
+        if(success):
+            print("AI: "+move)
+        else:
+            print("Error in AI!")
+        b.print_board()
+    elif choice2==3:
         resunmv=b.undo_move()
         if(resunmv==False):
             print("Undo Failed!")
         else:
             b.print_board()
-    if choice2==3:
+    elif choice2==4:
         curr_fen=b.get_fen()
         print("FEN of Current Game State: ",curr_fen)
-    if choice2==4:
+    elif choice2==5:
         new_fen=str(input("Enter FEN: "))
         b.load_fen(new_fen)
         b.print_board()
